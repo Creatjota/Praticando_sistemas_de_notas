136a55df051bf1bd878d32dfd5baf6bdf0f259a2
Creatjota <jv-oliveira07@hotmail.com>
2021-10-11 12:52:11 -0300

    Atualizando codigo

--- nota_escolares.js
+++ nota_escolares.js
@@ -1,13 +1,13 @@
-let nota = 101
+let nota = 89.5561561561561564
 if (nota >= 90 && nota <= 100) {
     console.log("Tipo A")
-} else if (nota >= 80 && nota <= 89) {
+} else if (nota >= 80 && nota < 90) {
     console.log("Tipo B")
-} else if (nota > 69 && nota < 80) {
+} else if (nota >= 70 && nota < 80) {
     console.log("Tipo C")
-} else if (nota >= 61 && nota < 70) {
+} else if (nota >= 60 && nota < 70) {
     console.log("Tipo D")
-} else if (nota <= 60) {
+} else if (nota < 60) {
     console.log("Tipo F")
 } else {
     console.log("Nota invalida")

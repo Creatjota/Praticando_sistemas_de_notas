6387013b7a9f675660a4ca08d7f3c350e1259efd
Creatjota <jv-oliveira07@hotmail.com>
2021-10-11 12:03:09 -0300

    Fiz um treino/ Sistemas de notas/

---
+++ nota_escolares.js
@@ -0,0 +1,14 @@
+let nota = 101
+if (nota >= 90 && nota <= 100) {
+    console.log("Tipo A")
+} else if (nota >= 80 && nota <= 89) {
+    console.log("Tipo B")
+} else if (nota > 69 && nota < 80) {
+    console.log("Tipo C")
+} else if (nota >= 61 && nota < 70) {
+    console.log("Tipo D")
+} else if (nota <= 60) {
+    console.log("Tipo F")
+} else {
+    console.log("Nota invalida")
+}
